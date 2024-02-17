b52125d657210b1aa88414da79e09e3d67ee4a01
muna <munaalforqani4@gmail.com>
2024-02-17 23:56:40 +0400

    massege

---
+++ Centaxs.php
@@ -0,0 +1,48 @@
+<?php
+
+
+//echo 'Hello,  World! Welcome to PHP page ';
+//$message = "Hello";
+//echo $message;
+
+//$variable_name = "PHP Class";
+
+//$title = "PHP is awesome!";
+//echo $title;
+
+
+
+//define( 'ORIGIN', [0, 0] );
+
+////define('WIDTH','1140px');
+//echo ORIGIN;
+
+
+//const RGB = ['red', 'green', 'blue'];
+
+//const SALES_TAX = 0.085;
+
+//$gross_price = 100;
+
+//$net_price = $gross_price * (1 + SALES_TAX);
+
+//echo "RGB"; 
+
+
+
+$rate = 100;
+$hours = 173;
+$payout = $hours * $rate; // payout calculation
+echo $payout
+
+//define('PREFIX', 'OPTION');
+
+
+
+/*
+   This is an example of a multi-line comment,
+   which can span multiple lines.
+*/
+
+//$title = 'PHP comment'; # set default title
+?>
